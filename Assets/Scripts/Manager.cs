34e0a9853f947b383b39767fb0802e9dd6861d58
youngguen.ahn <dddksss@gmail.com>
2016-01-07 18:04:01 +0900

    Start Project 4 buttons Without Functions

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+	public int setcount = 0;
+	public static Manager instance = null;
+
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void SetCount()
+	{
+		setcount++;
+		Debug.Log (setcount.ToString ());
+	}
+}
---
+++ Assets/Scripts/Today.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Globalization;
+
+public class Today : MonoBehaviour {
+
+	UILabel daylabel;
+	public DateTime today;
+	public string text;
+
+	// Use this for initialization
+	void Start () {
+		daylabel = this.GetComponent<UILabel> ();
+		today = DateTime.Today;
+		text = today.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		daylabel.text = text;
+
+
+	}
+}
