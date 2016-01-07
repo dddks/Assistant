808f29e95442bcfb35dbcd6dd79b552eb000216d
youngguen.ahn <dddksss@gmail.com>
2016-01-07 18:25:18 +0900

    Create ButtonManager , Connect Scenes

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void OnClickCountBtn()
+	{
+		Application.LoadLevel ("Assistant_count");
+	}
+
+}
